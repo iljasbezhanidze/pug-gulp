a3e77f19558ca47b1de163235a81489598623ad3
Ilyas Bezhanidze <iljas.bezhanidze@totonis.com>
2022-02-05 10:38:48 +0200

    first commit

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,15 @@
-console.log('hui')
+var swiper = new Swiper(".mySwiper", {
+    // direction: "vertical",
+    slidesPerView: 1,
+    spaceBetween: 0,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+      },
+  });
+
+
+  
